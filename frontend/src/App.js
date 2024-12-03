4792c71b30529b723b6cd70c9d399870c0bcf024
Darys21 <yvesanguilet6@gmail.com>
2024-12-03 13:28:17 +0000

    Fix: Remove unused DraftPage import and add missing Babel dependency

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import DraftPage from './components/DraftPage';
 import AdminLogin from './components/AdminLogin';
 import styled from 'styled-components';
 import io from 'socket.io-client';
