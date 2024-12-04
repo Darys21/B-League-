3f406b84811af1e4cc59a17e15e9966a310d968c
Darys21 <yvesanguilet6@gmail.com>
2024-12-04 11:27:59 +0000

    Update backend port configuration to match Railway deployment

--- app.py
+++ app.py
@@ -257,5 +257,5 @@ def update_current_team():
 application = app
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 8080))
+    socketio.run(app, host='0.0.0.0', port=port, debug=False)
